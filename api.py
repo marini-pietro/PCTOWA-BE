d6d67eb7099cfd59e0e80448db22c3456bb10adf
Pietro Marini <pmarini72107@gmail.com>
2025-03-13 13:36:04 +0100

    begins writing api

--- api.py
+++ api.py
@@ -1,22 +1,47 @@
 from flask import Flask, jsonify, request
-import sqlite3
+import mysql.connector
 
-def get_db_connection():
-    conn = sqlite3.connect('database.db')
-    conn.row_factory = sqlite3.Row
-    return conn
-
+conn = mysql.connector.connect(
+        host='172.16.1.98',
+        user='pctowa',
+        password='pctowa2025',
+        database='pctowa'
+        )
 app = Flask(__name__)
-conn = get_db_connection()
 
+# User login related
 @app.route('/user_login', methods=['GET'])
 def user_login():
     username = request.args.get('username')
     password = request.args.get('password')
 
-    users = conn.execute('SELECT * FROM users').fetchall()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute('SELECT * FROM users WHERE username = %s AND password = %s', (username, password))
+    if cursor.rowcount == 1: 
+        return jsonify({"outcome": "user with provided credentials exists"})
+    else: 
+        return jsonify({"outcome": "error, user with provided credentials already exists"})
+    
+@app.route('/user_register', methods=['GET'])
+def user_register():
+    username = request.args.get('username')
+    password = request.args.get('password')
+    type = request.args.get('type')
+
+    cursor = conn.cursor(dictionary=True)
+    
+    # Find out if user already exists
+    cursor.execute('SELECT * FROM users WHERE username = %s AND password = %s', (username, password))
+    if cursor.rowcount != 1:
+        return jsonify({"outcome": "error, user with provided credentials already exists"}) # If it does return error data
+    else:
+        # If not 
+        cursor.execute('INSERT INTO users VALUES (%s, %s, %s)', (username, password, type))
+        return jsonify({"outcome": "user successfully created"})
+    
+def close_api():
     conn.close()
-    return jsonify([dict(user) for user in users])
+    exit
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True) # Debug uguale a True per facilitare lo sviluppo andrebbe messo a false in produzione
